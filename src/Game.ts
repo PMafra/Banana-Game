5c98c8d0d7bec64ec62cf3bc3cdce4f67bf90eaa
PMafra <pmafrarm@gmail.com>
2022-01-09 16:46:50 -0300

    Create player image with canvas; add event listener to move player; add event listener to resize game canvas screen; create Player Class (still not using it)

--- src/Game.ts
+++ src/Game.ts
@@ -8,7 +8,11 @@ export class Game {
     gameLoopInterval: ReturnType<typeof setInterval> | string;
     turnInterval: ReturnType<typeof setInterval> | string;
 
+    player: HTMLImageElement;
+
     FPS: number;
+
+    playerX: number;
 
     constructor (canvas: HTMLCanvasElement, screenWidth: number, screenHeight: number, context: CanvasRenderingContext2D) {
         this.canvas = canvas;
@@ -19,7 +23,23 @@ export class Game {
         this.gameLoopInterval;
         this.turnInterval;
 
+        this.player;
+        this.playerX = 0;
+
         this.FPS = 60;
+
+        this.setupGame();
+    }
+
+    setupGame () {
+        this.canvas.width = this.screenWidth;
+        this.canvas.height = this.screenHeight;
+    }
+
+    updateScreenSize (width: number, heigth: number) {
+        this.screenWidth = width;
+        this.screenHeight = heigth;
+        this.setupGame();
     }
 
     turn () {
@@ -30,12 +50,27 @@ export class Game {
     }
 
     clearScreen () {
-        this.context.clearRect(0,0,this.canvas.width,this.canvas.height);
+        this.context.clearRect(0,0,this.screenWidth,this.screenHeight);
+    }
+
+    drawPlayer () {
+        this.player = new Image();
+        this.player.src = './images/alien.png';
+        this.context.drawImage(this.player, this.playerX, this.screenHeight - 126, 60, 100)
+    }
+
+    movePlayer (event: KeyboardEvent) {
+        if (event.key === 'a' && this.playerX >= 5) {
+            this.playerX -= 5;
+        }
+        if (event.key === 'd' && this.playerX <= this.screenWidth - 60) {
+            this.playerX += 5;
+        }
     }
 
     gameLoop () {
         this.clearScreen();
-
+        this.drawPlayer();
         this.turn();
     }
 
---
+++ src/Player.ts
@@ -0,0 +1,27 @@
+export class Player {
+
+    context: CanvasRenderingContext2D;
+    src: string;
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    image: HTMLImageElement;
+
+    constructor (context: CanvasRenderingContext2D, src:string, width: number, height: number) {
+        this.context = context;
+        this.src = src;
+        this.width = width
+        this.height = height;
+        this.image;
+
+        this.x;
+        this.y;
+    }
+
+    draw(x: number, screenHeight: number){
+        this.image = new Image();
+        this.image.src = this.src;
+        this.context.drawImage(this.image, x, screenHeight - (26+this.height), this.width, this.height)
+    }
+}
